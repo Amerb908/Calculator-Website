c705670938c60e09246a22f86120d81d4e9c5511
Amerb908 <ab1571514@gmail.com>
2023-07-30 21:41:34 -0700

    Add a . on the calculator

--- script.js
+++ script.js
@@ -4,34 +4,34 @@ let previousOperator;
 
 const screen = document.querySelector('.screen');
 
-function buttonClick(value){
-    if(isNaN(value)){
+function buttonClick(value) {
+    if (isNaN(value)) {
         handleSymbol(value);
-    }else{
+    } else {
         handleNumber(value);
     }
     screen.innerText = buffer;
 }
 
-function handleSymbol(symbol){
-    switch(symbol){
+function handleSymbol(symbol) {
+    switch (symbol) {
         case "C":
             buffer = '0';
             runningTotal = 0;
             break;
         case '=':
-            if(previousOperator === null){
+            if (previousOperator === null) {
                 return;
             }
-            flushOperation(parseInt(buffer));
+            flushOperation(parseFloat(buffer));
             previousOperator = null;
             buffer = runningTotal;
             runningTotal = 0;
             break;
         case '←':
-            if (buffer.length === 1){
+            if (buffer.length === 1) {
                 buffer = '0';
-            }else{
+            } else {
                 buffer = buffer.slice(0, buffer.length - 1);
             }
             break;
@@ -41,53 +41,72 @@ function handleSymbol(symbol){
         case '÷':
             handleMath(symbol);
             break;
+        case '.':
+            // Allow only one dot in the buffer
+            if (!buffer.includes('.')) {
+                buffer += symbol;
+            }
     }
 }
 
-function handleMath(symbol){
-    if (buffer === '0'){
+function handleMath(symbol) {
+    if (buffer === '0') {
         return;
     }
 
-    const intBuffer = parseInt(buffer);
+    const floatBuffer = parseFloat(buffer);
 
-    if (runningTotal === 0){
-        runningTotal = intBuffer;
-    }else{
-        flushOperation(intBuffer);
+    if (runningTotal === 0) {
+        runningTotal = floatBuffer;
+    } else {
+        flushOperation(floatBuffer);
     }
     previousOperator = symbol;
     buffer = '0';
 }
 
-function flushOperation(intBuffer) {
+function flushOperation(floatBuffer) {
     if (previousOperator === '+') {
-        runningTotal += intBuffer;
+        runningTotal += floatBuffer;
     } else if (previousOperator === '-') {
-        runningTotal -= intBuffer;
+        runningTotal -= floatBuffer;
     } else if (previousOperator === 'x') {
-        runningTotal *= intBuffer;
+        runningTotal *= floatBuffer;
     } else if (previousOperator === '÷') {
         // Perform division
-        runningTotal /= intBuffer;
+        runningTotal /= floatBuffer;
         // Limit the result to two decimal places without rounding
-        runningTotal = Number(runningTotal.toFixed(2));
     }
+    runningTotal = parseFloat(runningTotal.toFixed(2));
 }
 
+function handleNumber(numberString) {
+    // Allow adding the decimal point if it hasn't been used yet and the buffer is not empty
+    if (numberString === '.') {
+        if (buffer.includes('.')) {
+            // Avoid adding multiple decimal points
+            return;
+        }
+        if (buffer === '') {
+            buffer = '0';
+        }
+    }
 
-
-function handleNumber(numberString){
-    if(buffer === "0"){
+    if (buffer === "0") {
         buffer = numberString;
-    }else{
+    } else {
+        // Limit the number of decimal places to two
+        if (numberString === '.' && buffer.includes('.') && buffer.split('.')[1].length >= 2) {
+            return;
+        }
         buffer += numberString;
     }
 }
 
-function init(){
-    document.querySelector('.calc-buttons').addEventListener('click', function(event){
+function init() {
+    document.querySelector('.calc-buttons').addEventListener('click', function (event) {
         buttonClick(event.target.innerText);
     });
 }
+
 init();
